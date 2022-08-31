83f154c77872a6dbaca660c1c1bda1c854101720
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-08-31 18:49:53 +0530

    iconbar done

--- src/Layout/IconBar.js
+++ src/Layout/IconBar.js
@@ -2,7 +2,9 @@ import React from "react";
 import { VscFiles } from "react-icons/vsc";
 import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { VscAccount } from "react-icons/vsc";
+import { VscSettingsGear } from "react-icons/vsc";
 
 const icons = [
 	{
@@ -25,23 +27,37 @@ const icons = [
 const IconBar = () => {
 	const { pathname } = useLocation();
 	return (
-		<ul className="flex flex-col w-16 bg-iconbar-blue text-gray-500 text-xl">
-			{icons.map((icon) => {
-				return (
-					<Link
-						to={icon.page}
-						key={icon.id}
-						className={`flex justify-center items-center py-3 px-2 hover:text-white border-l
+		<div className="flex flex-col w-16 justify-between bg-iconbar-blue text-gray-500 text-xl">
+			<ul className="flex flex-col">
+				{icons.map((icon) => {
+					return (
+						<Link
+							to={icon.page}
+							key={icon.id}
+							className={`flex justify-center items-center py-3 px-2 hover:text-white border-l
 						${
 							pathname === icon.page
 								? "border-l-white text-white"
 								: "border-l-iconbar-blue"
 						}`}>
-						{icon.icon}
-					</Link>
-				);
-			})}
-		</ul>
+							{icon.icon}
+						</Link>
+					);
+				})}
+			</ul>
+			<ul className="flex flex-col">
+				<li
+					className={`flex justify-center items-center py-3 px-2 hover:text-white 
+					border-l border-l-iconbar-blue cursor-pointer`}>
+					<VscAccount />
+				</li>
+				<li
+					className={`flex justify-center items-center py-3 px-2 hover:text-white
+					border-l border-l-iconbar-blue cursor-pointer`}>
+					<VscSettingsGear />
+				</li>
+			</ul>
+		</div>
 	);
 };
 
